88e221c54c76e330f13a547807657496da3fce6f
Md Jahid Hasan <jahid.facts@gmail.com>
2023-08-09 18:44:28 +0600

    update in app

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
-require('dotenv').config(); // Import dotenv at the beginning
-
+// Import required packages
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const cors = require('cors'); // Import the CORS package
+require('dotenv').config(); // Load environment variables from .env file
 
+// Create the Express app
 const app = express();
+
+// Set up port for the server
 const port = process.env.PORT || 3000;
+
+// Set up database connection URL
 const dbURL = process.env.MONGODB_URL || 'mongodb://localhost:27017/airbnb';
 
+// Use CORS middleware
+app.use(cors({
+  origin: 'http://localhost:3000', // Replace with your frontend domain
+  methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+  credentials: true, // Allow cookies to be sent along with requests
+}));
+
+// Use body-parser, helmet, morgan, and cookie-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('combined'));
+app.use(cookieParser());
+
+// Connect to MongoDB
 mongoose.connect(dbURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// Set up connection events
 const connection = mongoose.connection;
 
 connection.on('connected', () => {
   console.log('Connected to MongoDB');
+  
+  // Start the server only after the database connection is established
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 });
 
 connection.on('error', (err) => {
@@ -30,23 +57,15 @@ connection.on('disconnected', () => {
   console.log('Disconnected from MongoDB');
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(morgan('combined'));
-app.use(cookieParser());
-
+// Import and use userRoute
 const userRoute = require('./src/routes/userRoute');
 app.use('/api', userRoute);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
+// Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(err.statusCode || 500).json({
-      success: false,
-      error: err.message || 'Internal Server Error',
-    });
+  console.error(err.stack);
+  res.status(err.statusCode || 500).json({
+    success: false,
+    error: err.message || 'Internal Server Error',
+  });
 });
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/userModel'); 
 const sendToken = require('../utils/jwtToken');
 
@@ -6,18 +7,19 @@ exports.registerUser = async (req, res, next) => {
   const { name, email, password } = req.body;
 
   try {
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
       avatar: {
-        public_id: 'avatar/1', // Change this to your desired value
-        url: 'Image name',   // Change this to your desired value
+        public_id: 'avatar/1',
+        url: 'Image name',  
       },
     });
 
     sendToken(user, 201, res); // Use sendToken to send the JWT token
   } catch (error) {
-    next(error); // Pass the error to the error handling middleware
+    next(error); 
   }
 };
